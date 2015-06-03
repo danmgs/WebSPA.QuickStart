6cd8a86609d8e20847ace60c2a717933aad66c98
danmgs <dnlnguyen@gmail.com>
2015-06-03 21:53:39 +0200

    Commit
    Commit

---
+++ WebSPA.QuickStart/Controllers/GarageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebSPA.QuickStart.Models;
+
+namespace WebSPA.QuickStart.Controllers
+{
+    public class GarageController : ApiController
+    {
+        public MongoDbContext Context = new MongoDbContext();
+
+        [HttpGet]
+        public List<Car> GetAllCars()
+        {
+            var result = new List<string>();
+            return Context.Cars.FindAll().ToList<Car>();
+        }
+
+        [HttpPost]
+        public void InsertCarsData(int count)
+        {
+            Random rnd = new Random();            
+
+            for(var i = 1; i <= count; i++)
+            {
+                var randomNum = rnd.Next();
+                Context.Cars.Save<Car>(new Car() { MaxSpeed = randomNum, Color = (randomNum%2 == 0)?"Blue":"Red" });
+            }            
+        }   
+    }
+}
---
+++ WebSPA.QuickStart/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSPA.QuickStart.Models
+{
+    public class Car
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Color { get; set; }
+        public double MaxSpeed { get; set; }
+        public int Year { get; set; }
+        public string OriginCountry { get; set; }
+    }
+}
